c19e59848c9138448e127da1252e7c01fe281f64
nischayv <nischay.v@gmail.com>
2017-10-01 17:21:45 -0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "linebreak-style": "off",
+    "comma-dangle": "off",
+    "no-console": "off",
+    "consistent-return": "off"
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 #! /usr/bin/env node
-console.log("Hello, Hammer!");
+console.log("Hello, Hammer!")
