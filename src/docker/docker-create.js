0b84f84347e4fa3c2171e6fbc72b86aced67b5cc
Jack  Meyer <jackmeyerdev@gmail.com>
2017-10-08 20:29:28 -0500

    moved file contents to the constants file and removed unecessary logs

--- src/docker/docker-create.js
+++ src/docker/docker-create.js
@@ -2,39 +2,16 @@ import fs from 'fs';
 
 import constants from '../constants/constants'
 
-const dockerFileContents = `# Use the official Node runtime as a parent image
-# More info at https://hub.docker.com/_/node/
-FROM node:alpine
-    
-# Set the working directory
-WORKDIR /usr/src/app
-
-# Install app dependencies
-COPY package.json package-lock.json ./
-RUN npm install
-
-# Bundle app source
-COPY . .
-
-# Make port 8080 available to the world outside this container
-EXPOSE 8080
-
-# Run "npm start" when the container launches
-CMD ["npm", "start"]
-`;
-
 export default function initDocker(config) {
-  console.log('Creating Dockerfile and .dockerignore...');
-
-  fs.writeFileSync(`${config.projectName}/${constants.docker.dockerFile.fileName}`, dockerFileContents, (err) => {
+  fs.writeFileSync(`${config.projectName}/${constants.docker.dockerFile.fileName}`,
+    constants.docker.dockerFile.fileContents, (err) => {
     if (err) {
       console.log(`${constants.docker.dockerFile.error.fileWrite}\n${err.toString()}`);
     }
   });
 
-  const dockerIgnoreContents = 'node_modules\nnpm-debug.log\n';
-
-  fs.writeFileSync(`${config.projectName}/${constants.docker.dockerIgnore.fileName}`, dockerIgnoreContents, (err) => {
+  fs.writeFileSync(`${config.projectName}/${constants.docker.dockerIgnore.fileName}`,
+    constants.docker.dockerIgnore.fileContents, (err) => {
     if (err) {
       console.log(`${constants.docker.dockerIgnore.error.fileWrite}\n${err.toString()}`);
     }
--- src/travis-ci/travis-ci-create.js
+++ src/travis-ci/travis-ci-create.js
@@ -2,9 +2,7 @@ import fs from 'fs';
 import constants from '../constants/constants';
 
 export default function initTravisCI(config) {
-  console.log('Creating .travis.yml...');
-
-  fs.writeFileSync(`${config.projectName}/${constants.travisCI.fileName}`, constants.travisCI.fileConents, (err) => {
+  fs.writeFileSync(`${config.projectName}/${constants.travisCI.fileName}`, constants.travisCI.fileContents, (err) => {
     if (err) {
       console.log(`${constants.travisCI.error.fileWrite}\n${err.toString()}`);
     }
