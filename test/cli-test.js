ef7a5b2298d5b3e64967f71412278039826012ac
Jack  Meyer <jackmeyerdev@gmail.com>
2017-10-09 12:02:37 -0500

    better grouping of test cases and added before/after to eliminate duplicate code

--- test/cli-test.js
+++ test/cli-test.js
@@ -17,125 +17,58 @@ const configs = {
   container: 'Docker'
 };
 
-describe('Init Project', () => {
-  describe('#initProject()', () => {
+describe('Initialize Project Files', () => {
+  before(() => {
+    cli.initProject(configs);
+  });
+
+  describe('Initialize Project Directories', () => {
     it('should create a new directory with the project name', () => {
-
-
-      cli.initProject(configs);
       assert.equal(true, fs.existsSync(configs.projectName));
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
-  });
-});
 
-describe('Init Project', () => {
-  describe('#initProject()', () => {
     it('should create a src directory underneath the project directory', () => {
-      cli.initProject(configs);
       assert.equal(true, fs.existsSync(configs.projectName + '/src'));
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
-  });
-});
 
-describe('Init Project', () => {
-  describe('#initProject()', () => {
     it('should return an error if a directory with the project name already exists', () => {
-
-      cli.initProject(configs);
       var result = cli.initProject(configs); // run initProject again to see if it fails
       assert.equal(constants.config.projectName.error.duplicateMessage, result);
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
   });
-});
 
-describe('Init Project', () => {
-  describe('#initTravisCI()', () => {
+  describe('Initialize Travis CI', () => {
     it('should create a .travis.yml file', () => {
-
-      cli.initProject(configs);
       assert.equal(true, fs.existsSync(`${configs.projectName}/.travis.yml`));
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
-  });
-});
 
-describe('Init Project', () => {
-  describe('#initTravisCI()', () => {
     it('should create a .travis.yml file with the proper contents', () => {
-
-      cli.initProject(configs);
       var contents = fs.readFileSync(`${configs.projectName}/.travis.yml`);
       assert.equal(constants.travisCI.fileContents, contents);
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
   });
-});
 
-describe('Init Project', () => {
-  describe('#initDocker()', () => {
+  describe('Initialize Docker', () => {
     it('should create a Dockerfile and .dockerignore', () => {
-
-      cli.initProject(configs);
       assert.equal(true, fs.existsSync(`${configs.projectName}/Dockerfile`));
       assert.equal(true, fs.existsSync(`${configs.projectName}/.dockerignore`));
-
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
-  });
-});
 
-describe('Init Project', () => {
-  describe('#initDocker()', () => {
     it('should create a Dockerfile and .dockerignore with the proper contents', () => {
-
-      cli.initProject(configs);
       var dockerContents = fs.readFileSync(`${configs.projectName}/Dockerfile`);
       var dockerignoreContents = fs.readFileSync(`${configs.projectName}/.dockerignore`);
 
       assert.equal(constants.docker.dockerFile.fileContents, dockerContents);
       assert.equal(constants.docker.dockerIgnore.fileContents, dockerignoreContents);
-
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
   });
-});
 
-describe('Init Project', () => {
-  describe('#createPackageJson()', () => {
+  describe('Initialize package.json', () => {
     it('should create a package.json file', () => {
-
-      cli.initProject(configs);
       assert.equal(true, fs.existsSync(`${configs.projectName}/package.json`));
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
-  });
-});
 
-describe('Init Project', () => {
-  describe('#createPackageJson()', () => {
     it('should create a package.json file with the proper contents', () => {
-
-      cli.initProject(configs);
-
       const packageJsonExpectedContents = '{\n' +
         '\t"name": "jack",\n' +
         '\t"version": "0.0.1",\n' +
@@ -154,43 +87,26 @@ describe('Init Project', () => {
         '}';
 
 
-      var  packageJsonActualContents= fs.readFileSync(`${configs.projectName}/package.json`);
+      var packageJsonActualContents = fs.readFileSync(`${configs.projectName}/package.json`);
       assert.equal(packageJsonExpectedContents, packageJsonActualContents);
-
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
   });
-});
 
-describe('Init Project', () => {
-  describe('#createIndexFile()', () => {
+  describe('Initialize index.js', () => {
     it('should create an index.js file', () => {
-
-      cli.initProject(configs);
       assert.equal(true, fs.existsSync(`${configs.projectName}/src/index.js`));
-
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
-  });
-});
 
-describe('Init Project', () => {
-  describe('#createIndexFile()', () => {
     it('should create an index.js file with the proper contents', () => {
-
-      cli.initProject(configs);
       var indexJsContents = fs.readFileSync(`${configs.projectName}/src/index.js`);
-
       assert.equal(constants.indexJS.fileContents, indexJsContents);
-
-      // clean up directory that was created
-      fsExtra.removeSync(configs.projectName);
     });
   });
+
+  after(() => {
+    fsExtra.removeSync(configs.projectName);
+  });
 });
+
 
 
