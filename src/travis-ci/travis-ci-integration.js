d30fd2b877467b8e95ca7d130450be73344d9370
KrashLeviathan <nathankarasch@gmail.com>
2017-10-09 12:13:38 -0500

    Got Travis activation working with HTTP requests

--- src/travis-ci/travis-ci-integration.js
+++ src/travis-ci/travis-ci-integration.js
@@ -1,36 +1,86 @@
 import inquirer from 'inquirer';
 
 const superagent = require('superagent');
+const winston = require('winston');
 
-const hammerAgent = 'HammerCLI/1.0.0';
+const hammerAgent = 'Travis/1.0';
 let travisAccessToken = '';
 
+function activateTravisHook(repositoryId) {
+  winston.log('info', 'activateTravisHook', {
+    repositoryId
+  });
+  superagent
+    .put('https://api.travis-ci.org/hooks')
+    .send({
+      hook: {
+        id: repositoryId,
+        active: true
+      }
+    })
+    .set({
+      'Content-Type': 'application/json',
+      'User-Agent': hammerAgent,
+      Accept: 'application/vnd.travis-ci.2+json',
+      Authorization: `token ${travisAccessToken}`
+    })
+    .end((err) => {
+      if (err) { return console.log(err); }
+      winston.log('info', 'activateTravisHook', 'SUCCESS!');
+    });
+}
+
+function getRepositoryId(config) {
+  winston.log('info', 'getRepositoryId', {
+    username: config.username,
+    projectName: config.projectName
+  });
+  superagent
+    .get(`https://api.travis-ci.org/repos/${config.username}/${config.projectName}`)
+    .set({
+      'User-Agent': hammerAgent,
+      Accept: 'application/vnd.travis-ci.2+json',
+      Authorization: `token ${travisAccessToken}`
+    })
+    .end((err, res) => {
+      if (err) { return console.log(err); }
+      activateTravisHook(res.body.repo.id);
+    });
+}
+
 function deleteGitHubToken(githubToken, url, config) {
+  winston.log('info', 'deleteGitHubToken', {
+    username: config.username,
+    projectName: config.projectName
+  });
   superagent
     .delete(url)
     .send({ githubToken })
     .set({ Authorization: `Basic ${Buffer.from(`${config.username}:${config.passw}`).toString('base64')}` })
     .end((err) => {
       if (err) { return console.log(err); }
-      console.log('Finished! Ready to do more travis stuff with my new shiny token.');
+      getRepositoryId(config);
     });
 }
 
 function requestTravisToken(githubToken, url, config) {
+  winston.log('info', 'requestTravisToken', {
+    username: config.username,
+    projectName: config.projectName
+  });
   superagent
     .post('https://api.travis-ci.org/auth/github')
     .send({ github_token: githubToken })
     .set({
       'Content-Type': 'application/json',
       'User-Agent': hammerAgent,
-      Accept: 'application/vnd.travis-ci.2+json',
-      'Content-Length': 37
+      Accept: 'application/vnd.travis-ci.2+json'
     })
     .end((err, res) => {
       if (err) { return console.log(err); }
 
       // Store the TravisCI access token
-      travisAccessToken = res.access_token;
+      travisAccessToken = res.body.access_token;
 
       // Delete the GitHub token via the GitHub API.
       deleteGitHubToken(githubToken, url, config)
@@ -38,6 +88,10 @@ function requestTravisToken(githubToken, url, config) {
 }
 
 function requestGitHubToken(config) {
+  winston.log('info', 'requestGitHubToken', {
+    username: config.username,
+    projectName: config.projectName
+  });
   // Create a GitHub token via the GitHub API, store GitHub token and URL.
   superagent
     .post('https://api.github.com/authorizations')
@@ -51,54 +105,13 @@ function requestGitHubToken(config) {
     })
     .set({
       'Content-Type': 'application/json',
-      Authorzation: `Basic ${Buffer.from(`${config.username}:${config.passw}`).toString('base64')}`
+      Authorization: `Basic ${Buffer.from(`${config.username}:${config.passw}`).toString('base64')}`
     })
     .end((err, res) => {
       if (err) { return console.log(err); }
-
-      // Store token and url
-      const { github_token } = res.token;
-      const { url } = res.url;
 
       // Use the /auth/github endpoint to exchange it for an access token. Store the access token.
-      requestTravisToken(github_token, url, config);
-    });
-}
-
-function activateTravisHook(repositoryId) {
-  superagent
-    .put('https://api.travis-ci.org/hooks')
-    .send({
-      hook: {
-        id: repositoryId,
-        active: true
-      }
-    })
-    .set({
-      'Content-Type': 'application/json',
-      'User-Agent': hammerAgent,
-      Accept: 'application/vnd.travis-ci.2+json',
-      'Content-Length': 37,
-      Authorization: `token ${travisAccessToken}`
-    })
-    .end((err) => {
-      if (err) { return console.log(err); }
-
-      console.log('SUCCESS');
-    });
-}
-
-function getRepositoryId(config) {
-  superagent
-    .get(`https://api.travis-ci.org/repos/${config.username}/${config.projectName}`)
-    .set({
-      'User-Agent': hammerAgent,
-      Accept: 'application/vnd.travis-ci.2+json',
-      Authorization: `token ${travisAccessToken}`
-    })
-    .end((err, res) => {
-      if (err) { return console.log(err); }
-      activateTravisHook(res.repo.id);
+      requestTravisToken(res.body.token, res.body.url, config);
     });
 }
 
@@ -116,6 +129,5 @@ export default function enableTravisOnProject(username, projectName) {
       passw: answers.passw
     };
     requestGitHubToken(config);
-    getRepositoryId(config);
   });
 }
