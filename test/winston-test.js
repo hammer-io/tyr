1443465f082e8ba4a878534a875d747e538ba176
KrashLeviathan <nathankarasch@gmail.com>
2017-10-24 14:34:38 -0500

    Added tests for logging to file

--- test/winston-test.js
+++ test/winston-test.js
@@ -1,6 +1,7 @@
 import assert from 'assert';
+import fs from 'fs-extra';
 
-import { setActiveLogger, getActiveLogger } from '../src/utils/winston';
+import { enableLogFile, setActiveLogger, getActiveLogger } from '../src/utils/winston';
 import { beforeEach, afterEach } from 'mocha';
 
 // Test strategy for capturing console output found here:
@@ -28,12 +29,13 @@ function captureStream(stream){
 
 function logAllLevels(logger) {
   // NOTE: We can't capture stderr during mocha tests (which
-  // 'error' and 'debug' are written to)
-  // logger.error('error');
+  // 'error' and 'debug' are written to) unless they are
+  // logged to a file.
+  logger.error('error');
   logger.warn('warn');
   logger.info('info');
   logger.verbose('verbose');
-  // logger.debug('debug');
+  logger.debug('debug');
   logger.silly('silly');
 }
 
@@ -74,5 +76,31 @@ describe('Winston:', () => {
     // NOTE: 'debug' level is also written to stderr
     // See https://github.com/winstonjs/winston/pull/558
     assert.equal(hook.captured(), expected);
+  });
+
+  it('Should log to file if logging to file is enabled', (done) => {
+    const logFileName = 'tyr-test.log';
+    setActiveLogger('debug');
+    enableLogFile(logFileName);
+
+    // This happens in a tricky asynchronous way that can't be waited for,
+    // so we'll check the results in a timeout.
+    logAllLevels(getActiveLogger());
+
+    setTimeout(() => {
+      assert.equal(fs.existsSync(logFileName), true, `The log file '${logFileName}' does not exist!`);
+
+      const actualContents = fs.readFileSync(logFileName);
+      const containsLogs = (
+        actualContents.includes('[ERROR  ] error\n') &&
+        actualContents.includes('[WARN   ] warn\n') &&
+        actualContents.includes('[INFO   ] info\n') &&
+        actualContents.includes('[DEBUG  ] debug\n')
+      );
+      assert.ok(containsLogs, 'The contents of the log file don\'t match what\'s expected!');
+
+      fs.removeSync(logFileName);
+      done();
+    }, 1000);
   });
 });
