8210b95968e3e6bad24c8d9de8159b23eb31cd60
Jack Meyer <jackcmeyer@gmail.com>
2017-12-19 19:18:20 -0600

    added tests for parsing from config file

--- test/project-configuration-service.test.js
+++ test/project-configuration-service.test.js
@@ -1,4 +1,5 @@
 import assert from 'assert';
+import fs from 'fs';
 
 import { parseConfigsFromFile } from '../dist/services/project-configuration-service';
 import { getActiveLogger } from '../dist/utils/log/winston';
@@ -8,16 +9,21 @@ const log = getActiveLogger();
 describe('Project Configuration Service', () => {
   describe('Read from Configuration File', () => {
     it('should parse configurations without issues', () =>{
+      try {
+        assert.equal(
+          JSON.parse(fs.readFileSync('test/test-configurations/valid-project-configuration')),
+          parseConfigsFromFile('test/test-configurations/valid-project-configuration'));
+      } catch (error) {
 
+      }
     });
 
-    it('should parse configurations and throw an error when an error occurrs', () => {
+    it('should parse configurations and throw an error when an error occurs', () => {
       try {
         parseConfigsFromFile('test/test-configurations/bad-format-configuration');
       } catch (error) {
-        log.error(error.message);
+        assert.equal(error.message, 'Invalid configuration file format!');
       }
-
     });
   });
 });
