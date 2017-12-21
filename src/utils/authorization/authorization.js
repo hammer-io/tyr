a8f0ed7269f56bee971ca4089e5238712002e0fe
Jack Meyer <jackcmeyer@gmail.com>
2017-12-20 23:32:39 -0600

    removed unused functions

--- src/utils/authorization/authorization.js
+++ src/utils/authorization/authorization.js
@@ -8,23 +8,3 @@
 export function basicAuthorization(username, password) {
   return `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`;
 }
-
-/**
- * Returns the string used for the token authorization header\t
- *
- * @param token
- * @returns {string}
- */
-export function tokenAuthorization(token) {
-  return `token ${token}`;
-}
-
-/**
- * Returns the string used for the bearer authorization header\t
- *
- * @param token
- * @returns {string}
- */
-export function bearerAuthorization(token) {
-  return `Bearer ${token}`;
-}
