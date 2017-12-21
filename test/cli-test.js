093465ad9895c06bbb3bf4fbeaca17abdb5c1f4c
Jack Meyer <jackcmeyer@gmail.com>
2017-12-21 00:06:55 -0600

    added tests for sign ins

--- test/cli-test.js
+++ test/cli-test.js
@@ -1,22 +1,73 @@
+import assert from 'assert';
+import sinon from 'sinon';
+
+import * as cli from './../dist/cli';
+import * as herokuService from './../dist/services/heroku-service';
+import * as githubService from './../dist/services/github-service';
+import * as prompt from './../dist/prompt/prompt';
+
+
 describe('Test CLI Functionality', () => {
 
-  describe('Sign in to GitHub', () => {
-    it('should sign in to GitHub and return the credentials', () => {
+  describe('signInToGithub()', () => {
 
+    let githubStub = sinon.stub(githubService, 'isValidCredentials');
+    let promptStub = sinon.stub(prompt, 'promptForGithubCredentials');
+    let spy = sinon.spy(cli, 'signInToGithub');
+
+    it('should sign in to GitHub and return the credentials', async () => {
+      promptStub.resolves({username: 'blah', password: 'blah'});
+      githubStub.resolves(true);
+
+      const credentials = await cli.signInToGithub();
+      assert.equal(credentials.username, 'blah');
+      assert.equal(credentials.password, 'blah');
+      assert.equal(spy.calledOnce, true);
     });
 
-    it('should reprompt the user if the credentials are invalid', () =>  {
+    it('should reprompt the user if the credentials are invalid', async () =>  {
+      promptStub
+        .onFirstCall().resolves({username: 'blah', password: 'blah'})
+        .onSecondCall().resolves({username: 'blah', password: 'blah'});
 
+      githubStub
+        .onFirstCall().resolves(false)
+        .onSecondCall().resolves(true);
+
+      const credentials = await cli.signInToGithub();
+      assert.equal(credentials.username, 'blah');
+      assert.equal(credentials.password, 'blah');
     });
   });
 
-  describe('Sign in to Heroku', () => {
-    it('should sign in to Heroku and return the credentials', () => {
+  describe('signInToHeroku()', () => {
+    let herokuStub = sinon.stub(herokuService, 'isValidCredentials');
+    let promptStub = sinon.stub(prompt, 'promptForHerokuCredentials');
+    let spy = sinon.spy(cli, 'signInToHeroku');
 
+    it('should sign in to Heroku and return the credentials', async () => {
+      promptStub.resolves({email: 'blah@blah.com', password: 'blah'});
+      herokuStub.resolves(true);
+
+      const credentials = await cli.signInToHeroku();
+      assert.equal(credentials.email, 'blah@blah.com');
+      assert.equal(credentials.password, 'blah');
+      assert.equal(spy.calledOnce, true);
     });
 
-    it('should reprompt the user if the credentials are invalid', () =>  {
+    it('should reprompt the user if the credentials are invalid', async () =>  {
 
+      promptStub
+        .onFirstCall().resolves({username: 'blah', password: 'blah'})
+        .onSecondCall().resolves({username: 'blah', password: 'blah'});
+
+      herokuStub
+        .onFirstCall().resolves(false)
+        .onSecondCall().resolves(true);
+
+      const credentials = await cli.signInToHeroku();
+      assert.equal(credentials.username, 'blah');
+      assert.equal(credentials.password, 'blah');
     });
   });
 });
