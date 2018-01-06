371856bca09a1317db609fe28572add0418a06e1
Jack Meyer <jackcmeyer@gmail.com>
2018-01-06 00:01:44 -0600

    test for enable travis

--- test/travis-service-test.js
+++ test/travis-service-test.js
@@ -1,13 +1,185 @@
 import assert from 'assert';
+import sinon from 'sinon';
 import fs from 'fs-extra';
 
-import {generateTravisCIFile} from '../dist/services/travis-service';
+import * as githubClient from '../dist/clients/github';
+import * as travisClient from '../dist/clients/travis';
+
+import * as travisService from '../dist/services/travis-service';
+import {generateTravisCIFile, enableTravis} from '../dist/services/travis-service';
 
 describe('Test Travis Service', () => {
-    describe('generateTravisCIFile', () => {
-      beforeEach(() => {
-        fs.mkdirSync('test-project');
-      });
+  let configs = {};
+  let githubTokenRequest = {};
+  let travisTokenRequest = {};
+  let travisAccountRequest = {};
+  let travisGetUserInformationRequest = {};
+  let syncTravisWithGithubRequest = {};
+  let getRepositoryIdRequest = {};
+  let activateTravisHookRequest = {};
+  let setEnvironmentVariableRequest = {};
+  let deleteGithubTokenRequest = {};
+  let waitForSync = {};
+
+
+  describe('enableTravis()', () => {
+    beforeEach(() => {
+      configs = JSON.parse(fs.readFileSync('test/test-configurations/valid-project-configurations-credentials'));
+      githubTokenRequest = sinon.stub(githubClient, 'requestGitHubToken');
+      travisTokenRequest = sinon.stub(travisClient, 'requestTravisToken');
+      travisAccountRequest = sinon.stub(travisClient, 'getUserAccount');
+      travisGetUserInformationRequest = sinon.stub(travisClient, 'getUserInformation');
+      syncTravisWithGithubRequest = sinon.stub(travisClient, 'syncTravisWithGithub');
+      getRepositoryIdRequest = sinon.stub(travisClient, 'getRepositoryId');
+      activateTravisHookRequest = sinon.stub(travisClient, 'activateTravisHook');
+      setEnvironmentVariableRequest = sinon.stub(travisClient, 'setEnvironmentVariable');
+      deleteGithubTokenRequest = sinon.stub(githubClient, 'deleteGitHubToken');
+      waitForSync = sinon.stub(travisService, 'waitForSync');
+    });
+
+
+    it('should enable Travis CI for a user', async () => {
+      githubTokenRequest.resolves("1234");
+      travisTokenRequest.resolves("1234");
+      travisAccountRequest.resolves({accounts: [
+          {id: 1, login: 'blah'},
+          {id: 2, login: 'something else'}
+        ]});
+      waitForSync.resolves();
+      travisGetUserInformationRequest.resolves({user: {user_is_syncing: false}});
+      syncTravisWithGithubRequest.resolves();
+      getRepositoryIdRequest.resolves(1);
+      activateTravisHookRequest.resolves();
+      setEnvironmentVariableRequest.resolves();
+
+      const token = await enableTravis(configs);
+      assert.equal(token, '1234');
+    }).timeout(100000);
+
+    it('should throw an error if the GitHub token request fails', async () => {
+      githubTokenRequest.rejects();
+
+      try {
+        await enableTravis(configs);
+        assert.equal(true, false, 'should throw an error');
+      } catch (error) {
+        assert.equal(error.message, "Failed to enable travis on blah/test because we were unable to get a token from GitHub.")
+      }
+    });
+
+    it('should throw an error if the Travis token request fails', async () => {
+      githubTokenRequest.resolves("1234");
+      travisTokenRequest.rejects();
+
+      try {
+        await enableTravis(configs);
+        assert.equal(true, false, 'should throw an error');
+      } catch (error) {
+        assert.equal(error.message, "Failed to enable travis on blah/test" +
+          " because we were unable to get a token from TravisCI.");
+      }
+    });
+
+    it('should thrown an error if the User Account request fails', async () => {
+      githubTokenRequest.resolves("1234");
+      travisTokenRequest.resolves("1234");
+      travisAccountRequest.rejects();
+
+      try {
+        await enableTravis(configs);
+        assert.equal(true, false, 'should throw an error');
+      } catch (error) {
+        assert.equal(error.message, "Failed to enable travis on blah/test because we were unable" +
+          " to get account information from TravisCI.")
+      }
+    });
+
+    it('should throw an error if Travis cannot sync with GitHub', async () => {
+      githubTokenRequest.resolves("1234");
+      travisTokenRequest.resolves("1234");
+      travisAccountRequest.resolves({accounts: [
+          {id: 1, login: 'blah'},
+          {id: 2, login: 'something else'}
+        ]});
+      waitForSync.resolves();
+      travisGetUserInformationRequest.resolves({user: {user_is_syncing: false}});
+      syncTravisWithGithubRequest.rejects();
+
+      try {
+        await enableTravis(configs);
+        assert.equal(true, false, 'should throw an error');
+      } catch (error) {
+        assert.equal(error.message, "Failed to enable travis on blah/test because we were unable" +
+          " to sync TravisCI with GitHub.")
+      }
+
+
+    }).timeout(100000);
+
+    it('should throw an error if Travis Hook cannot be activiated on the repository', async () => {
+      githubTokenRequest.resolves("1234");
+      travisTokenRequest.resolves("1234");
+      travisAccountRequest.resolves({accounts: [
+          {id: 1, login: 'blah'},
+          {id: 2, login: 'something else'}
+        ]});
+      waitForSync.resolves();
+      travisGetUserInformationRequest.resolves({user: {user_is_syncing: false}});
+      syncTravisWithGithubRequest.resolves();
+      getRepositoryIdRequest.resolves(1);
+      activateTravisHookRequest.rejects();
+
+      try {
+        await enableTravis(configs);
+        assert.equal(true, false, 'should throw an error');
+      } catch (error) {
+        assert.equal(error.message, "Failed to enable travis on blah/test because we were unable" +
+          " to activate TravisCI.")
+      }
+    }).timeout(100000);
+
+    it('should throw an error if environment variables cannot be set', async () => {
+      githubTokenRequest.resolves("1234");
+      travisTokenRequest.resolves("1234");
+      travisAccountRequest.resolves({accounts: [
+          {id: 1, login: 'blah'},
+          {id: 2, login: 'something else'}
+        ]});
+      waitForSync.resolves();
+      travisGetUserInformationRequest.resolves({user: {user_is_syncing: false}});
+      syncTravisWithGithubRequest.resolves();
+      getRepositoryIdRequest.resolves(1);
+      activateTravisHookRequest.resolves();
+      setEnvironmentVariableRequest.rejects();
+
+      try {
+        await enableTravis(configs);
+        assert.equal(true, false, 'should throw an error');
+      } catch (error) {
+        assert.equal(error.message, "Failed to enable travis on blah/test because we were unable" +
+          " to set environment variables.")
+      }
+
+    }).timeout(100000);
+
+    afterEach(() => {
+      githubTokenRequest.restore();
+      travisTokenRequest.restore();
+      travisAccountRequest.restore();
+      travisGetUserInformationRequest.restore();
+      syncTravisWithGithubRequest.restore();
+      getRepositoryIdRequest.restore();
+      activateTravisHookRequest.restore();
+      setEnvironmentVariableRequest.restore();
+      deleteGithubTokenRequest.restore();
+      waitForSync.restore();
+    });
+  });
+
+  describe('generateTravisCIFile', () => {
+    beforeEach(() => {
+      fs.mkdirSync('test-project');
+    });
 
     it('should generate a basic .travis.yml file since heroku/docker was not selected', async () => {
       const configs = JSON.parse("{\n" +
